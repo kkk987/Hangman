346d6a22c726dd567a6e915b589e5aea8403cd32
kkk987 <markying21549@gmail.com>
2019-04-27 12:45:08 +1000

    game class initialized

---
+++ game.rb
@@ -0,0 +1,42 @@
+# Hangman - Game class 
+# Created on 25/04/2019
+# Functionalities: 
+# - Initialize the game setting
+# - Attributes: 
+# - Behabiors: check if game finished, display game interface, 
+#              show help menu
+
+class Game
+    def initialize
+        
+    end
+    #show avaible menu here
+    def Display_Menu
+        
+    end
+    #load a new secret word
+    def New_Game
+        
+    end
+    #Check if game is finished
+    # 2 situations finish the game: 
+    #  - correctly guess the secret word
+    #  - guess chances is zero
+    #  - -q is entered
+    def Gameover?
+        
+    end
+    #explain the game rules and availbe commands
+    def Help_Menu
+        
+    end
+    #quit the game
+    def Quit
+        
+    end
+    #all error messages are put here
+    def Error_Msg
+        
+    end
+    
+end
